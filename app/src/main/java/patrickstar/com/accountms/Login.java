3799afc2f8c2d3b2546bafaf8706321ab1434122
ljmweb <1240465012@qq.com>
2017-09-14 11:58:27 +0800

    修改初始界面为登录界面

--- app/src/main/java/patrickstar/com/accountms/Login.java
+++ app/src/main/java/patrickstar/com/accountms/Login.java
@@ -11,6 +11,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import patrickstar.com.accountms.model.tb_pwd;
+
+import patrickstar.com.accountms.db.DBOpenHelper;
+import patrickstar.com.accountms.db.DBPwd;
+import patrickstar.com.accountms.db.DBPwd;
 
 public class Login extends AppCompatActivity {
 
@@ -18,14 +23,22 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
+        DBOpenHelper helper=new DBOpenHelper(Login.this);
+        helper.getWritableDatabase();
+        String str="";
+        DBPwd dao=new DBPwd(Login.this);
+        int count=dao.getCount();
+        if(count<1){
+            dao.insert("123456");
+            Toast.makeText(Login.this,"初始密码为：123456",Toast.LENGTH_LONG);
+        }
         final EditText txtlogin=(EditText)findViewById(R.id.txtLogin);  //获取密码文本框
         Button btnlogin=(Button)findViewById(R.id.btnLogin);            //获取"登录"按钮
         btnlogin.setOnClickListener(new View.OnClickListener() {        //为"登录"按钮设置监听事件
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Login.this,MainActivity.class);//创建Intent对象
-                PwdDAO pwdDAO=new PwdDAO(Login.this);                   //创建PwdDAO对象
+                DBPwd pwdDAO=new DBPwd(Login.this);                   //创建PwdDAO对象
 
                 //判断是否有密码及是否输入了密码
                 if((pwdDAO.getCount()==0| pwdDAO.find().getPassword().isEmpty())&&
