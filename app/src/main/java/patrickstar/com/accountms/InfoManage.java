3d14b59a3527c329eb2e55b9ea7ab7201e484b16
Smallmile <3066814532@qq.com>
2017-09-16 09:27:49 +0800

    标签业务类测试1

--- app/src/main/java/patrickstar/com/accountms/InfoManage.java
+++ app/src/main/java/patrickstar/com/accountms/InfoManage.java
@@ -1,8 +1,12 @@
 package patrickstar.com.accountms;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,13 +24,13 @@ import patrickstar.com.accountms.model.tb_outaccount;
  */
 
 public class InfoManage extends Activity {
-    protected  static final int DATE_DIALOG_ID=0;//创建日期对话框常量
-    TextView tvtitle,textView;//创建两个TextView对象
-    EditText txtMoney,txtTime,txtHA,txtMark;//创建四个EditText对象
+    protected static final int DATE_DIALOG_ID = 0;//创建日期对话框常量
+    TextView tvtitle, textView;//创建两个TextView对象
+    EditText txtMoney, txtTime, txtHA, txtMark;//创建四个EditText对象
     Spinner spType;//创建Spinner对象
-    Button btnEdit,btnDel,btninoutBack;//创建两个Button对象
-    String [] strInfos;//定义字符串数组
-    String strid,strType;//定义两个字符串变量，分别用来记录信息编号和管理类型
+    Button btnEdit, btnDel, btninoutBack;//创建两个Button对象
+    String[] strInfos;//定义字符串数组
+    String strid, strType;//定义两个字符串变量，分别用来记录信息编号和管理类型
     private int mYear;//年
     private int mMonth;//月
     private int mDay;//日
@@ -36,50 +40,43 @@ public class InfoManage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.infomanage);
-        btninoutBack=(Button)findViewById(R.id.btnInOutBack);//获取返回按钮
-        final DBOutAccount outaccountDAO=new DBOutAccount(InfoManage.this);//创建DBOutAccount对象
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.infomanage);
+        btninoutBack = (Button) findViewById(R.id.btnInOutBack);//获取返回按钮
+        final DBOutAccount outaccountDAO = new DBOutAccount(InfoManage.this);//创建DBOutAccount对象
         final DBInAcount inAcount = new DBInAcount(InfoManage.this);
-       // Toast.makeText(InfoManage.this, "进入",Toast.LENGTH_LONG).show();
-        tvtitle=(TextView)findViewById(R.id.inouttitle);//获取标题标签对象
-        textView=(TextView)findViewById(R.id.tvInOut) ;//获取"地点/付款方式"标签对象
-        txtMoney=(EditText)findViewById(R.id.txtInOutMoney);//获取"金额"文本框
-        txtTime=(EditText)findViewById(R.id. txtInOutTime);//获取"时间"文本框
-        txtMark=(EditText)findViewById(R.id.txtInOutMark);//获取"备注"文本框
-        txtHA=(EditText)findViewById(R.id.txtInOut) ;//获取"地点/付款方式"文本框对象
-        spType=(Spinner)findViewById(R.id.spInOutType);//获取"类别"下拉列表
-        btnEdit=(Button)findViewById(R.id.btnInOutEdit);//获取"修改"按钮
-        btnDel=(Button)findViewById(R.id.btnInOutDelete);//获取"删除"按钮
-        Intent intent=getIntent();//创建Intent对象
-         info= intent.getStringExtra("info");
+        tvtitle = (TextView) findViewById(R.id.inouttitle);//获取标题标签对象
+        textView = (TextView) findViewById(R.id.tvInOut);//获取"地点/付款方式"标签对象
+        txtMoney = (EditText) findViewById(R.id.txtInOutMoney);//获取"金额"文本框
+        txtTime = (EditText) findViewById(R.id.txtInOutTime);//获取"时间"文本框
+        txtMark = (EditText) findViewById(R.id.txtInOutMark);//获取"备注"文本框
+        txtHA = (EditText) findViewById(R.id.txtInOut);//获取"地点/付款方式"文本框对象
+        spType = (Spinner) findViewById(R.id.spInOutType);//获取"类别"下拉列表
+        btnEdit = (Button) findViewById(R.id.btnInOutEdit);//获取"修改"按钮
+        btnDel = (Button) findViewById(R.id.btnInOutDelete);//获取"删除"按钮
+        Intent intent = getIntent();//创建Intent对象
+        info = intent.getStringExtra("info");
 
 
-        //Toast.makeText(InfoManage.this, info,Toast.LENGTH_LONG).show();
-        //Bundle budle=intent.getExtras();//获取传入的数据，并使用Bundle对象
-        //strInfos=budle.getStringArray(Showinfo.FLAG);//获取Bundle中记录的信息
         strInfos = info.split(",");
-        strid=strInfos[0];
-        strType=strInfos[1];//记录类型
-       if(strType.equals("btnoutinfo"))
-        {
+        strid = strInfos[0];
+        strType = strInfos[1];//记录类型
+        if (strType.equals("btnoutinfo")) {
             tvtitle.setText("支出管理");//设置标题为"支出管理"
             textView.setText("地点：");//设置"地点/付款方"标签文本为"地点："
             //根据编号查找支出信息，并存储到tb_outaccount对象中
-             tboutaccount=new DBOutAccount(this).find(Long.parseLong(strid));
-            txtMoney.setText(String.valueOf(tboutaccount.getMoney()) );//显示金额
+            tboutaccount = new DBOutAccount(this).find(Long.parseLong(strid));
+            txtMoney.setText(String.valueOf(tboutaccount.getMoney()));//显示金额
             txtTime.setText(tboutaccount.getTime());//显示时间
             spType.setPrompt(tboutaccount.getType());//显示类别
             txtHA.setText(tboutaccount.getAddress());//显示地点
             txtMark.setText(tboutaccount.getMark());//显示备注
-        }
-        else if(strType.equals("btnininfo"))
-        {
+        } else if (strType.equals("btnininfo")) {
             tvtitle.setText("收入管理");//设置标题为"收入管理"
             textView.setText("付款方：");//设置"地点/付款方"标签文本为"付款方："
             //根据编号查找支出信息，并存储到tb_outaccount对象中
-             inaccount=new DBInAcount(this).find(Integer.parseInt(strid));
-            txtMoney.setText(String.valueOf(inaccount.getMoney()) );//显示金额
+            inaccount = new DBInAcount(this).find(Integer.parseInt(strid));
+            txtMoney.setText(String.valueOf(inaccount.getMoney()));//显示金额
             txtTime.setText(inaccount.getTime());//显示时间
             spType.setPrompt(inaccount.getType());//显示类别
             txtHA.setText(inaccount.getHandler());//显示付款方
@@ -89,62 +86,73 @@ public class InfoManage extends Activity {
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(strType.equals("btnoutinfo"))//判断类型如果是btnoutinfo
+                if (strType.equals("btnoutinfo"))//判断类型如果是btnoutinfo
                 {
-
-                    //tboutaccount.set_id(Integer.parseInt(strid));//设置编号
-                    tboutaccount.setMoney(Double.parseDouble(txtMoney.getText().toString()) );//设置金额
+                    tboutaccount.setMoney(Double.parseDouble(txtMoney.getText().toString()));//设置金额
                     tboutaccount.setTime(txtTime.getText().toString());//设置时间
                     tboutaccount.setType(spType.getSelectedItem().toString());//设置类别
                     tboutaccount.setAddress(txtHA.getText().toString());//设置地点
                     tboutaccount.setMark(txtMark.getText().toString());//设置备注
-                    if(outaccountDAO.update(tboutaccount)){
-                        Toast.makeText(InfoManage.this,"【数据】修改成功！",Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(InfoManage.this,"【数据】修改失败！",Toast.LENGTH_SHORT).show();
+                    if (outaccountDAO.update(tboutaccount)) {
+                        Toast.makeText(InfoManage.this, "【数据】修改成功！", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(InfoManage.this, "【数据】修改失败！", Toast.LENGTH_SHORT).show();
 
-                    };//更新支出信息
-                }
-                else if(strType.equals("btnininfo")){
-                    //tb_inaccount tbinaccount=new  tb_inaccount();//创建tb_outaccount对象
-                    //tbinaccount.set_id(Integer.parseInt(strid));//设置编号
+                    }
+                    ;//更新支出信息
+                } else if (strType.equals("btnininfo")) {
 
-                    inaccount.setMoney(Double.parseDouble(txtMoney.getText().toString()) );//设置金额
+                    inaccount.setMoney(Double.parseDouble(txtMoney.getText().toString()));//设置金额
                     inaccount.setTime(txtTime.getText().toString());//设置时间
                     inaccount.setType(spType.getSelectedItem().toString());//设置类别
                     inaccount.setHandler(txtHA.getText().toString());//设置付款方
                     inaccount.setMark(txtMark.getText().toString());//设置备注
-                    if(inAcount.update(inaccount)){
-                        Toast.makeText(InfoManage.this,"【数据】修改成功！",Toast.LENGTH_SHORT).show();
-                    }else{
-                        Toast.makeText(InfoManage.this,"【数据】修改失败！",Toast.LENGTH_SHORT).show();
-                    };//更新收入信息
+                    if (inAcount.update(inaccount)) {
+                        Toast.makeText(InfoManage.this, "【数据】修改成功！", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(InfoManage.this, "【数据】修改失败！", Toast.LENGTH_SHORT).show();
+                    }
+                    ;//更新收入信息
                 }
-                //弹出信息提示
-               // Toast.makeText(InfoManage.this,"【数据】修改成功！",Toast.LENGTH_SHORT).show();
+
             }
         });
         btnDel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(strType.equals("btnininfo"))
-                {
-                   if( inAcount.deleteById(Integer.parseInt(strid))){
-                       Toast.makeText(InfoManage.this,"【数据】删除成功！",Toast.LENGTH_SHORT).show();
-                   }
-                   else{
-                       Toast.makeText(InfoManage.this,"【数据】删除失败！",Toast.LENGTH_SHORT).show();
-                   }
-                }
-                else if(strType.equals("btnoutinfo"))
-                {
-                    if(outaccountDAO.deleteById(Long.parseLong(strid))){
-                        Toast.makeText(InfoManage.this,"【数据】删除成功！",Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(InfoManage.this);
+                builder.setTitle("确认");
+                builder.setMessage("确认删除该记录？");
+                builder.setPositiveButton("删除", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (strType.equals("btnininfo")) {
+
+                            if (!inAcount.deleteById(Integer.parseInt(strid))) {
+                                //String result = inAcount.deleteById(Integer.parseInt(strid));
+                                Toast.makeText(InfoManage.this, "【数据】删除成功！", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(InfoManage.this,fl_MainActivity.class));
+                                //finish();
+                            } else {
+                                Toast.makeText(InfoManage.this, "【数据】删除失败！", Toast.LENGTH_SHORT).show();
+                            }
+                        } else if (strType.equals("btnoutinfo")) {
+                            if (outaccountDAO.deleteById(Long.parseLong(strid))) {
+                                Toast.makeText(InfoManage.this, "【数据】删除成功！", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(InfoManage.this,fl_MainActivity.class));
+                                fl_MainActivity.test();
+
+                            } else {
+                                Toast.makeText(InfoManage.this, "【数据】删除失败！", Toast.LENGTH_SHORT).show();
+                            }
+                        }
                     }
-                    else{
-                        Toast.makeText(InfoManage.this,"【数据】删除失败！",Toast.LENGTH_SHORT).show();
-                    }
-                }
+                });
+                builder.setNegativeButton("取消", null);
+                builder.show();
+
+
+
 
             }
         });
@@ -156,5 +164,6 @@ public class InfoManage extends Activity {
         });
 
     }
+
 
 }
--- app/src/main/java/patrickstar/com/accountms/fl_MainActivity.java
+++ app/src/main/java/patrickstar/com/accountms/fl_MainActivity.java
@@ -20,6 +20,7 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import patrickstar.com.accountms.db.DBFlag;
 import patrickstar.com.accountms.db.DBInAcount;
@@ -31,7 +32,7 @@ import patrickstar.com.accountms.model.tb_outaccount;
 
 public class fl_MainActivity extends AppCompatActivity {
     private Button edit;
-    private ListView  listView;
+    private ListView listView;
     public List<tb_outaccount> oData;
     public List<tb_inaccount> iData;
     public List<tb_flag> fData;
@@ -41,15 +42,18 @@ public class fl_MainActivity extends AppCompatActivity {
     private Button shouru;
     private Button bianqian;
     public String[] tbsize;
-    ArrayAdapter<String> adapter =null;
-    public static final  String FLAG= "id";
+    ArrayAdapter<String> adapter = null;
+    public static final String FLAG = "id";
     public TextView title;
-    String strType="";
+    String strType = "";
+    public static Button ZHICHU;
+    public static fl_MainActivity flMainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fl_activity_main);
+        flMainActivity = this;
 
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
@@ -62,98 +66,45 @@ public class fl_MainActivity extends AppCompatActivity {
 
         }
 
-        zhichu = (Button) findViewById(R.id.zhichu);
+        ZHICHU = zhichu = (Button) findViewById(R.id.zhichu);
         shouru = (Button) findViewById(R.id.shouru);
         bianqian = (Button) findViewById(R.id.bianqian);
         title = (TextView) findViewById(R.id.title);
-
-
-        //action 编辑按钮
-        //edit = (Button) findViewById(R.id.edit);
-        //  find(Integer.parseInt(strid));
-        //chk = (CheckBox) findViewById(R.id.chk1);
-       /* edit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(fl_MainActivity.this, "编辑", Toast.LENGTH_SHORT).show();
-                //chk.setChecked(true);
-            }
-        });*/
-
         listView = (ListView) findViewById(R.id.listview);
-
-
         iData = this.getInData();
-      /*  oData = this.getOutData();
-        fData = this.getFData();*/
 
-
-
-      DBInAcount acount=new DBInAcount(fl_MainActivity.this);
-        List<tb_inaccount> tb= acount.findAll();
+        DBInAcount acount = new DBInAcount(fl_MainActivity.this);
+        List<tb_inaccount> tb = acount.findAll();
         tbsize = new String[tb.size()];
-        strType ="btnininfo";
-        int  m = 0;
-        for(tb_inaccount inaccount:tb){
-            tbsize[m] = inaccount.getId()+"|"+inaccount.getType()+String.valueOf(inaccount.getMoney())+"元"+inaccount.getTime();
+        strType = "btnininfo";
+        int m = 0;
+        for (tb_inaccount inaccount : tb) {
+            tbsize[m] = inaccount.getId() + "|" + inaccount.getType() + String.valueOf(inaccount.getMoney()) + "元" + inaccount.getTime();
             m++;
         }
-      adapter=new ArrayAdapter<String>(fl_MainActivity.this,android.R.layout.simple_expandable_list_item_1,tbsize);
-      listView.setAdapter(adapter);
+        adapter = new ArrayAdapter<String>(fl_MainActivity.this, android.R.layout.simple_expandable_list_item_1, tbsize);
+        listView.setAdapter(adapter);
 
        /* myAdapter = new MyAdapter(this, iData, 0);
         listView.setAdapter(myAdapter);*/
 
-        zhichu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                title.setText("我的支出");
+        zhichu.setOnClickListener(new ZhichuListen());
 
-                strType = "btnoutinfo";
-                List<tb_outaccount> list = getOutData();
-                tbsize = new String [list.size()];
-                int  m = 0;
-                for(tb_outaccount outaccount:list){
-                    tbsize[m] = outaccount.getId()+"|"+outaccount.getType()+String.valueOf(outaccount.getMoney())+"元"+outaccount.getTime();
-                    m++;
-                }
-                adapter=new ArrayAdapter<String>(fl_MainActivity.this,android.R.layout.simple_expandable_list_item_1,tbsize);
-                listView.setAdapter(adapter);
-
-            }
-        });
-
-        shouru.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                title.setText("我的收入");
-                strType = "btnininfo";
-                List<tb_inaccount> tb= getInData();
-                tbsize = new String[tb.size()];
-                int  m = 0;
-                for(tb_inaccount inaccount:tb){
-                    tbsize[m] = inaccount.getId()+"|"+inaccount.getType()+String.valueOf(inaccount.getMoney())+"元"+inaccount.getTime();
-                    m++;
-                }
-                adapter=new ArrayAdapter<String>(fl_MainActivity.this,android.R.layout.simple_expandable_list_item_1,tbsize);
-                listView.setAdapter(adapter);
-
-            }
-        });
+        shouru.setOnClickListener(new Listen());
 
         bianqian.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 title.setText("我的便签");
                 strType = "btnflaginfo";
-                List<tb_flag> tb= getFData();
+                List<tb_flag> tb = getFData();
                 tbsize = new String[tb.size()];
-                int  m = 0;
-                for(tb_flag inaccount:tb){
-                    tbsize[m] = inaccount.getId()+"|"+inaccount.getFlag();
+                int m = 0;
+                for (tb_flag inaccount : tb) {
+                    tbsize[m] = inaccount.getId() + "|" + inaccount.getFlag();
                     m++;
                 }
-                adapter=new ArrayAdapter<String>(fl_MainActivity.this,android.R.layout.simple_expandable_list_item_1,tbsize);
+                adapter = new ArrayAdapter<String>(fl_MainActivity.this, android.R.layout.simple_expandable_list_item_1, tbsize);
                 listView.setAdapter(adapter);
 
             }
@@ -162,56 +113,108 @@ public class fl_MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String strinfo = String.valueOf(((TextView)view).getText());
-                String strid = strinfo.substring(0,strinfo.indexOf("|"));
-                Toast.makeText(fl_MainActivity.this, strid,Toast.LENGTH_LONG).show();
-                if(strType == "btnflaginfo"){
-                    Intent intent  = new Intent(fl_MainActivity.this,FlagActivity.class);
-                    intent.putExtra("info",strid);
+                String strinfo = String.valueOf(((TextView) view).getText());
+                String strid = strinfo.substring(0, strinfo.indexOf("|"));
+
+                if (strType == "btnflaginfo") {
+                    Intent intent = new Intent(fl_MainActivity.this, FlagActivity.class);
+                    intent.putExtra("info", strid);
                     startActivity(intent);
-                }else{
-                Intent intent = new Intent(fl_MainActivity.this,InfoManage.class);
-                intent.putExtra("info",strid+","+strType);
-                startActivity(intent);}
+                } else {
+                    Intent intent = new Intent(fl_MainActivity.this, InfoManage.class);
+                    intent.putExtra("info", strid + "," + strType);
+                    startActivity(intent);
+                }
             }
         });
 
 
+    }
+
+    //获取所有收入信息
+    public List<tb_inaccount> getInData() {
+        List<tb_inaccount> list = new ArrayList<tb_inaccount>();
+        DBInAcount dbInAcount = new DBInAcount(this);
+        list = dbInAcount.findAll();
+        return list;
+    }
+
+    //支出信息
+    public List<tb_outaccount> getOutData() {
+        List<tb_outaccount> list = new ArrayList<tb_outaccount>();
+        DBOutAccount dbOutAccount = new DBOutAccount(this);
+        list = dbOutAccount.findAll();
+        return list;
+    }
+
+    //便签信息
+    public List<tb_flag> getFData() {
+        List<tb_flag> list = new ArrayList<tb_flag>();
+        DBFlag dbFlag = new DBFlag(this);
+        list = dbFlag.query();
+        return list;
+    }
 
 
-         }
-        //获取所有收入信息
-        public List<tb_inaccount> getInData () {
-            List<tb_inaccount> list = new ArrayList<tb_inaccount>();
-            DBInAcount dbInAcount = new DBInAcount(this);
-            list = dbInAcount.findAll();
-            return list;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Toast.makeText(fl_MainActivity.this,"返回主界面",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, com.xiaoke.accountsoft.activity.MainActivity.class);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showInfo(){
+        title.setText("我的支出");
+
+        strType = "btnoutinfo";
+        List<tb_outaccount> list = getOutData();
+        tbsize = new String[list.size()];
+        int m = 0;
+        for (tb_outaccount outaccount : list) {
+            tbsize[m] = outaccount.getId() + "|" + outaccount.getType() + String.valueOf(outaccount.getMoney()) + "元" + outaccount.getTime();
+            m++;
         }
+        adapter = new ArrayAdapter<String>(fl_MainActivity.this, android.R.layout.simple_expandable_list_item_1, tbsize);
+        listView.setAdapter(adapter);
+    }
 
-        //支出信息
-        public List<tb_outaccount> getOutData () {
-            List<tb_outaccount> list = new ArrayList<tb_outaccount>();
-            DBOutAccount dbOutAccount = new DBOutAccount(this);
-            list  = dbOutAccount.findAll();
-            return list;
-        }
+    public static void test() {
+        flMainActivity.showInfo();
+    }
 
-        //便签信息
-        public List<tb_flag> getFData () {
-            List<tb_flag> list = new ArrayList<tb_flag>();
-            DBFlag dbFlag = new DBFlag(this);
-            list  = dbFlag.query();
-            return list;
-        }
-
-
+    public class Listen implements View.OnClickListener {
         @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            //Toast.makeText(fl_MainActivity.this,"返回主界面",Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this,com.xiaoke.accountsoft.activity.MainActivity.class);
-            startActivity(intent);
-            return super.onOptionsItemSelected(item);
+        public void onClick(View view) {
+            title.setText("我的收入");
+            strType = "btnininfo";
+            List<tb_inaccount> tb = getInData();
+            tbsize = new String[tb.size()];
+            int m = 0;
+            for (tb_inaccount inaccount : tb) {
+                tbsize[m] = inaccount.getId() + "|" + inaccount.getType() + String.valueOf(inaccount.getMoney()) + "元" + inaccount.getTime();
+                m++;
+            }
+            adapter = new ArrayAdapter<String>(fl_MainActivity.this, android.R.layout.simple_expandable_list_item_1, tbsize);
+            listView.setAdapter(adapter);
         }
+    }
 
+    public class ZhichuListen implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            title.setText("我的支出");
 
+            strType = "btnoutinfo";
+            List<tb_outaccount> list = getOutData();
+            tbsize = new String[list.size()];
+            int m = 0;
+            for (tb_outaccount outaccount : list) {
+                tbsize[m] = outaccount.getId() + "|" + outaccount.getType() + String.valueOf(outaccount.getMoney()) + "元" + outaccount.getTime();
+                m++;
+            }
+            adapter = new ArrayAdapter<String>(fl_MainActivity.this, android.R.layout.simple_expandable_list_item_1, tbsize);
+            listView.setAdapter(adapter);
+        }
+    }
 }
